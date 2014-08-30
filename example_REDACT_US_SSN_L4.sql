7fb1698c1a301661da5f3d4aba105315f5af74e6
Johan Louwers <louwersj@gmail.com>
2014-08-30 10:41:25 +0200

    Create example_REDACT_US_SSN_L4.sql

---
+++ example_REDACT_US_SSN_L4.sql
@@ -0,0 +1,28 @@
+/* NAME:
+ * example_REDACT_US_SSN_F5.sql
+ *
+ * DESC:
+ * Example of Oracl Data Redaction using REDACT_US_SSN_F5 to redact a social security number.
+ * 
+ * LOG:
+ * VERSION---DATE--------NAME-------------COMMENT
+ * 0.1       29AUG2014   Johan Louwers    Initial upload to github.com
+ *
+ * LICENSE:
+ * Copyright (C) 2014  Johan Louwers
+ * 
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
